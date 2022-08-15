2754dee18073eea528ae2fad6b59a5bc1fd2b265
januszpolak <janusz.polak@interia.pl>
2022-08-15 20:11:18 +0200

    create navigator to show geolocation in console.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-geolocation';
+
+
+  ngOnInit(){
+    navigator.geolocation.getCurrentPosition((position =>{
+      console.log(`latitude ${position.coords.latitude}, longitude ${position.coords.longitude}`)
+    }))
+  }
 }
