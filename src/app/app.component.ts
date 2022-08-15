1e3bf6847072105cd9123b5e14515f01efb3ab73
januszpolak <janusz.polak@interia.pl>
2022-08-15 20:58:03 +0200

    show position on main page of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,19 @@ export class AppComponent implements OnInit{
   title = 'angular-geolocation';
 
 
+  latitude: any 
+  longitude: any 
+
   ngOnInit(){
     navigator.geolocation.getCurrentPosition((position =>{
       console.log(`latitude ${position.coords.latitude}, longitude ${position.coords.longitude}`)
+
+
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
     }))
+
+    
+
   }
 }
